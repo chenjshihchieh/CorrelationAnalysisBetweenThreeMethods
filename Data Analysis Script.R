33370da1e50c46c5e1e62fb379eeb9a609e470b5
jackchen570 <chen.j.shihchieh@gmail.com>
2018-12-30 02:37:50 -0800

    Dec 30 2018

--- Data Analysis Script.R
+++ Data Analysis Script.R
@@ -8,25 +8,31 @@ data <- raw[,grep(paste(colkeep, collapse = "|"), names(raw))]
 Y1data <- data[!is.na(data$ChangeInLocation1), c("PID", "Age", "AverageFBBias", "AverageMCBias", "ChangeInLocation1", "ChangeInLocation2")]
 Y1dataNoFailControl <- subset(Y1data, ChangeInLocation2 == 1, select = -ChangeInLocation2)
 Y1dataNoFailControl <- apply(Y1dataNoFailControl, 2, as.numeric) 
-print(LittleMCAR(Y1data))
+Y1dataNoFailControl <- as.data.frame(Y1dataNoFailControl)
+#print(LittleMCAR(Y1data))
 #hist(Y1data$AverageFBBias)
 #hist(Y1data$AverageMCBias)
 sink(file = "SpearmanOutput.txt")
 print(paste("Total Paritcipants:", (nrow(Y1data))))
 print(paste("Participants who answered Control Question correctly:", nrow(Y1dataNoFailControl)))
 
-print("Spearman between AverageFBBias and ChangeInLocation1")
+cat("\n", "\n", "\n", "\n", "\n", "Spearman between AverageFBBias and ChangeInLocation1", "\n")
 cor.results <- cor.test(formula = ~ AverageFBBias + ChangeInLocation1, data = Y1dataNoFailControl, method = "spearman")
 print(cor.results)
 
-print("Spearman between AverageFBBias and ChangeInLocation1, Controlling for Age")
+cat("\n", "\n", "\n", "\n", "\n", "Spearman between AverageFBBias and ChangeInLocation1, Controlling for Age", "\n", "\n")
 part1.r <- partial.r(data = Y1dataNoFailControl, x = c("AverageFBBias", "ChangeInLocation1"), y = "Age", method = "spearman")
 part1.cor <- corr.p(part1.r, n = {nrow(Y1dataNoFailControl) - 1})
 print(part1.cor, short = F)  
 
-print("Spearman between AverageFBBias and ChangeInLocation1, Controlling for Age and AverageMCBias")
+cat("\n", "\n", "\n", "\n", "\n", "Spearman between AverageFBBias and ChangeInLocation1, Controlling for Age and AverageMCBias", "\n", "\n")
 part2.r <- partial.r(data = Y1dataNoFailControl, x = c("AverageFBBias", "ChangeInLocation1"), y = c("Age", "AverageMCBias"), method = "spearman")
 part2.cor <- corr.p(part2.r, n = {nrow(Y1dataNoFailControl) - 1})
 print(part2.cor, short = F) 
+
+cat("\n", "\n", "\n", "\n", "\n", "Spearman between AverageFBBias - AverageMCBias and ChangeInLocation1", "\n")
+Y1dataNoFailControl$Combined <- Y1dataNoFailControl$AverageFBBias - Y1dataNoFailControl$AverageMCBias
+combined.result <- cor.test(formula = ~ Combined + ChangeInLocation1, data = Y1dataNoFailControl, method = "spearman")
+print(combined.result, short = F) 
 
 sink()
--- Logistics Regression.R
+++ Logistics Regression.R
@@ -8,31 +8,32 @@ data <- raw[,grep(paste(colkeep, collapse = "|"), names(raw))]
 Y1data <- data[!is.na(data$ChangeInLocation1), c("PID", "Age", "AverageFBBias", "AverageMCBias", "ChangeInLocation1", "ChangeInLocation2")]
 Y1dataNoFailControl <- subset(Y1data, ChangeInLocation2 == 1, select = -ChangeInLocation2)
 Y1dataNoFailControl <- apply(Y1dataNoFailControl, 2, as.numeric)
-print(LittleMCAR(Y1data))
+Y1dataNoFailControl <- as.data.frame(Y1dataNoFailControl)
+#print(LittleMCAR(Y1data))
 #hist(Y1data$AverageFBBias)
 #hist(Y1data$AverageMCBias)
 
-sink("Regression.txt")
+sink("Logistic Regression.txt")
 paste("Total Paritcipants:", (nrow(Y1data)))
 paste("Participants who answered Control Question correctly:", nrow(Y1dataNoFailControl))
 
-Y1dataNoFailControl <- as.data.frame(Y1dataNoFailControl)
-print("Model for AverageFBBias and ChangeInLocation1")
+cat("\n", "\n", "\n", "\n", "\n", "Model for AverageFBBias and ChangeInLocation1", "\n")
 results1 <- glm(formula = ChangeInLocation1 ~ AverageFBBias, data = Y1dataNoFailControl, family = binomial)
 print(summary(results1), short = FALSE)
 
-print("Model for AverageFBBias and ChangeInLocation1, Controlling for Age")
+cat("\n", "\n", "\n", "\n", "\n", "Model for AverageFBBias and ChangeInLocation1, Controlling for Age", "\n")
 results2 <- glm(formula = ChangeInLocation1 ~ Age * AverageFBBias, data = Y1dataNoFailControl, family = binomial)
 print(summary(results2), short = FALSE)
 
-print("Model for AverageFBBias and ChangeInLocation1, Controlling for Age and AverageMCBias")
+cat("\n", "\n", "\n", "\n", "\n", "Model for AverageFBBias and ChangeInLocation1, Controlling for Age and AverageMCBias", "\n")
 results3 <- glm(formula = ChangeInLocation1 ~ (AverageMCBias + Age + AverageFBBias)^3, data = Y1dataNoFailControl, family = binomial)
 print(summary(results3), short = FALSE)
 
-print("Model for AverageFBBias and ChangeInLocation1, Controlling for AverageMCBias")
+cat("\n", "\n", "\n", "\n", "\n", "Model for AverageFBBias and ChangeInLocation1, Controlling for AverageMCBias", "\n")
 results4 <- glm(formula = ChangeInLocation1 ~ AverageMCBias * AverageFBBias, data = Y1dataNoFailControl, family = binomial)
 print(summary(results4), short = FALSE)
 
+cat("\n", "\n", "\n", "\n", "\n", "Model for AverageFBBias - AverageMCBias and ChangeInLocation1", "\n")
 Y1dataNoFailControl$Combined <- Y1dataNoFailControl$AverageFBBias - Y1dataNoFailControl$AverageMCBias
 results4 <- glm(formula = ChangeInLocation1 ~ Combined, data = Y1dataNoFailControl, family = binomial)
 print(summary(results4), short = FALSE)
